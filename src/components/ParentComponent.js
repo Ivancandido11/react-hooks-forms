1873fc203e5007aab003ff924a0451bdb0b6b2b2
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-06 14:27:20 -0400

    Completed Assignment

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react"
+import Form from "./Form"
+import DisplayData from "../DisplayData"
+
+const ParentComponent = () => {
+  const [firstName, setFirstName] = useState("John")
+  const [lastName, setLastName] = useState("Henry")
+
+  const handleFirstNameChange = (e) => setFirstName(e.target.value)
+  const handleLastNameChange = (e) => setLastName(e.target.value)
+
+  return (
+    <div>
+      <Form 
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData 
+        firstName={firstName}
+        lastName={lastName}
+      />
+    </div>
+  )
+}
+
+export default ParentComponent
